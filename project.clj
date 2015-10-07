ae62a3b148b1938500402b05d862c9fa05c83c4a
Mike Gallaher <pragsmike@gmail.com>
2015-10-07 04:30:44 -0400

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clawss "0.1.6"
+(defproject clawss "0.1.7-SNAPSHOT"
   :description "clawss: clojure wrapper for web-service security"
   :url "https://bitbucket.org/pragsmike/clawss.git"
   :license {:name "Eclipse Public License"
